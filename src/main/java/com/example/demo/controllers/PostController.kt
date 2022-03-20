55b89d2c08cf27c79a8203657407ae714afced6f
Vijay Patidar <vramotiya@gmail.com>
2022-03-20 12:37:25 +0530

    Integrated prometheus

--- src/main/java/com/example/demo/controllers/PostController.kt
+++ src/main/java/com/example/demo/controllers/PostController.kt
@@ -1,24 +1,20 @@
 package com.example.demo.controllers
 
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import com.example.demo.services.PostService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import com.example.demo.models.Post
-import reactor.core.publisher.Mono
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import com.example.demo.services.PostService
+import io.micrometer.core.annotation.Timed
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/posts")
-
+@Timed("Post.Controller")
 class PostController(private val postService: PostService):BaseController() {
 
     @PostMapping
-    fun createPost(@RequestBody post: Post?): Mono<Post> {
-        return postService.createPost(post!!)
+    fun createPost(@RequestBody post: Post): Mono<Post> {
+        return postService.createPost(post)
     }
 
     @GetMapping
---
+++ src/main/java/com/example/demo/models/PostHeader.kt
@@ -0,0 +1,5 @@
+package com.example.demo.models
+
+import java.util.*
+
+data class PostHeader(val postId:String,val title:String,val postedBy:String,val postedOn: Date?)
